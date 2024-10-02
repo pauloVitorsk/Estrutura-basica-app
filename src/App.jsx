2310bf4915cf5d4b49891966c3b648d45169c831
pauloVitorsk <paulovitorsouza4066@gmail.com>
2024-10-02 12:07:54 -0300

    estrutura basicas

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Body from './layouts/Body/Body';
+import Footer from './layouts/Footer/Footer';
+import Header from './layouts/Header/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Body/>
+            <Footer/>
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/layouts/Body/Body.jsx
@@ -0,0 +1,9 @@
+import "./Body.css"
+function Body(){
+    return(
+        <div className="App-body">
+            <h1>corpo</h1>
+        </div>
+    )
+}
+export default Body;
